9e7e4b8d92af1eebcd324874cb8d9227f7db43e9
huny6226 <50163320+huny6226@users.noreply.github.com>
2019-07-10 10:17:18 +0900

    내거아님

---
+++ semi_project/src/semi/servlet/member/MemberDto.java
@@ -0,0 +1,12 @@
+package semi.servlet.member;
+
+public class MemberDto {
+	private int id;
+	private String name;
+	private String pwd;
+	private String pwdDt;
+	private String status;
+	private String grade;
+	private String birth;
+	private String phone;
+}
